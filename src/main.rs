822c9b35014b73ebbcdd2e630054d1a5175141c0
shoda2718 <shoda2718@gmail.com>
2024-06-11 17:46:24 +0900

    [TEST IMPLEMENT] "add" instruction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-fn main() {
+// this code is a rough sketch
+
+//use std::fmt::Error;
+
+fn main() -> Result<(), AsmError>{
     let add = "add 1 to eax";
-    let mut token = tokenize(add);
-    println!("{:?}", read_add(token));
+    let token = tokenize(add);
+    println!("{}", gen_add(read_add(token)?));
+    Ok(())
+}
+
+#[derive(Debug)]
+enum AsmError {
+    SyntaxError(String),
+    
 }
 
 struct Token<'a> {
@@ -10,9 +21,16 @@ struct Token<'a> {
 }
 
 impl<'a> Token<'a> {
-    fn get(&self) -> &'a str{
+    fn get(&self) -> &'a str {
         return self.seq[self.location];
     }
+
+    fn pop_left(&mut self) -> &'a str{
+        let ret = self.get();
+        self.next();
+        ret
+    }
+
     fn next(&mut self) {
         self.location += 1;
     }
@@ -22,8 +40,8 @@ impl<'a> Token<'a> {
 fn tokenize<'a>(s: &'a str) -> Token<'a> {
     // todo: make this function general
     Token {
-    seq: s.split_whitespace().collect::<Vec<&str>>(),
-    location: 0,
+        seq: s.split_whitespace().collect::<Vec<&str>>(),
+        location: 0,
     }
 }
 
@@ -36,26 +54,31 @@ struct Add<'a> {
     to: Object<'a>,
 }
 
-fn read_to<'a>(mut token:Token<'a>) -> Object<'a> {
-    if token.get() == "to" {
-        token.next();
-        return token.get();
+fn read_to<'a>(mut token:Token<'a>) -> Result<Object<'a>, AsmError> {
+    let to = token.pop_left();
+    if to == "to" {
+        Ok(token.pop_left())
+    } else {
+        Err(AsmError::SyntaxError(format!("expected 'to', but found '{}'", to)))
     }
-    todo!();
 }
 
-fn read_add<'a>(mut token:Token<'a>) -> Add<'a> {
-    let mut object: Object<'a>;
-    let mut to: Object<'a>;
-    if token.get() == "add" {
-        token.next();
-        object = token.get();
-        token.next();
-        to = read_to(token);
-        return Add{
+fn read_add<'a>(mut token:Token<'a>) -> Result<Add<'a>, AsmError> {
+    let object: Object<'a>;
+    let to: Object<'a>;
+    let add = token.pop_left();
+    if add == "add" {
+        object = token.pop_left();
+        to = read_to(token)?;
+        Ok(Add {
             object: object,
             to: to
-        };
+        })
+    } else {
+        Err(AsmError::SyntaxError(format!("expected 'add', but found '{}'", add)))
     }
-    todo!();
+}
+
+fn gen_add<'a>(add: Add<'a>) -> String {
+    format!("add {}, {}", add.to, add.object)
 }
