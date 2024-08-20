c41516e0787db7438dd7c4aa438e836ac183385e
shoda2718 <shoda2718@gmail.com>
2024-08-20 16:34:21 +0900

    [BUG FIX]

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+prep:make-target.sh
+	./make-target.sh
+
 test:test/test.sh
 	./test/test.sh
 
-test-5cc:test/test2.sh 5cc
+test-5cc:test/test2.sh 5cc prep
 	./test/test2.sh
 
 5cc:
--- make-target.sh
+++ make-target.sh
@@ -1,3 +1,9 @@
 if [ ! -d target ]; then
     mkdir test/target
 fi
+
+if [ ! -d test/target ]; then
+    mkdir test/target
+    mkdir test/target/5cc
+    mkdir test/target/test1
+fi
